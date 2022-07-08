6e14f4d2e648ac4b6f00aa1d3ea13f62e0c0ec6c
Jackevansevo <[jack@evans.gb.net]>
2022-07-08 20:38:44 +0100

    Fix bug not showing subscribers on admin detail

--- feeds/admin.py
+++ feeds/admin.py
@@ -7,8 +7,25 @@ from .models import Category, Entry, Feed, Subscription
 @admin.register(Feed)
 class FeedAdmin(admin.ModelAdmin):
     list_display = ("title", "url", "etag", "last_modified", "subscribers")
-    fields = ("url", "title", "slug", "link", "etag", "last_modified", "subscribers")
-    readonly_fields = ("title", "link", "slug", "etag", "last_modified", "subscribers")
+    fields = (
+        "url",
+        "title",
+        "slug",
+        "link",
+        "etag",
+        "last_modified",
+        "subscribers",
+        "last_checked",
+    )
+    readonly_fields = (
+        "title",
+        "link",
+        "slug",
+        "etag",
+        "last_modified",
+        "subscribers",
+        "last_checked",
+    )
     search_fields = ["title", "url", "link", "slug"]
 
     def get_queryset(self, request):
