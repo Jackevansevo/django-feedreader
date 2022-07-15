8cb6e40b81a157fb036526f673fc5b23e10acbf5
Jackevansevo <[jack@evans.gb.net]>
2022-07-15 09:03:00 +0100

    Only touch the DB once when refreshing

--- feeds/tasks.py
+++ feeds/tasks.py
@@ -6,6 +6,7 @@ from celery import group, shared_task
 from dateutil import parser
 from django.db import transaction
 from django.db.models import Count
+from django.utils import timezone
 from django.utils.http import http_date
 
 from feeds.models import Category, Feed, Subscription
@@ -25,68 +26,60 @@ def add_subscription(url, category_name, user_id):
             )
         else:
             category = None
-        feed, _ = _refresh_or_create_feed(url)
+        feed, _, _ = _refresh_or_create_feed(url)
         Subscription.objects.get_or_create(
             feed=feed, user_id=user_id, category=category
         )
     return {"url": feed.get_absolute_url(), "link": feed.link}
 
 
-def _refresh_or_create_feed(url):
+def _refresh_or_create_feed(url, last_modified=None, etag=None):
     # TODO: Could we avoid multiple trips to the DB to get/update the feed
-
-    feed, created = Feed.objects.get_or_create(url=url)
 
     headers = {"User-Agent": USER_AGENT}
 
-    if feed.etag is not None:
-        headers["If-None-Match"] = feed.etag
-    if feed.last_modified is not None:
-        headers["If-Modified-Since"] = http_date(feed.last_modified.timestamp())
+    if etag is not None:
+        headers["If-None-Match"] = etag
+    if last_modified is not None:
+        headers["If-Modified-Since"] = http_date(last_modified.timestamp())
 
-    resp = httpx.get(feed.url, headers=headers, follow_redirects=True)
+    resp = httpx.get(url, headers=headers, follow_redirects=True)
+
+    if resp.status_code == 304:
+        return url, False, False
 
     parsed = feedparser.parse(resp)
 
     # TODO check if 'lasted_checked' auto_now field is updated on save with
     # updated_fields
 
-    update_fields = []
+    update_fields = {"last_checked": timezone.now()}
 
-    if created:
-        feed.link = parsed.feed.link
+    update_fields["link"] = parsed.feed.link
 
-        if parsed.feed.title != "":
-            feed.title = parsed.feed.title
-        else:
-            feed.title = urlparse(parsed.feed.link).netloc.lstrip("www.")
-
-        update_fields = ["title", "slug", "link"]
-
-    if resp.status_code == 304:
-        return feed, False
+    if parsed.feed.title != "":
+        update_fields["title"] = parsed.feed.title
+    else:
+        update_fields["title"] = urlparse(parsed.feed.link).netloc.lstrip("www.")
 
     # Temporary redirect
     if resp.history:
-        feed.url = resp.url
-        update_fields.append("url")
+        update_fields["url"] = resp.url
 
     # https://feedparser.readthedocs.io/en/latest/http-etag.html
 
     if resp.headers.get("etag"):
-        feed.etag = resp.headers["etag"]
-        update_fields.append("etag")
+        update_fields["etag"] = resp.headers["etag"]
     if resp.headers.get("last-modified"):
-        feed.last_modified = parser.parse(resp.headers["last-modified"])
-        update_fields.append("last_modified")
+        update_fields["last_modified"] = parser.parse(resp.headers["last-modified"])
 
-    if update_fields:
-        feed.save(update_fields=update_fields)
+    feed, created = Feed.objects.update_or_create(url=url, defaults=update_fields)
 
     if parsed.entries:
-        feed.add_new_entries(parsed.entries)
-
-    return feed, bool(parsed.entries)
+        updated = feed.add_new_entries(parsed.entries)
+        return feed, created, updated
+    else:
+        return feed, created, False
 
 
 @shared_task
@@ -94,22 +87,24 @@ def refresh_feeds():
     feeds = (
         Feed.objects.annotate(subscribers=Count("subscriptions"))
         .filter(subscribers__gt=0)
-        .values_list("url", flat=True)
+        .values_list("url", "last_modified", "etag")
     )
-    g = group(refresh_or_create_feed.s(url) for url in feeds)
-    res = g()
-    return res
+    return group(
+        refresh_or_create_feed.s(url, last_modified, etag)
+        for (url, last_modified, etag) in feeds
+    ).apply_async()
 
 
 @shared_task
-def refresh_or_create_feed(url):
-    feed, updated = _refresh_or_create_feed(url)
+def refresh_or_create_feed(url, last_modified=None, etag=None):
+    feed, created, updated = _refresh_or_create_feed(url)
     return {
         "feed": {
             "url": feed.url,
             "last_modified": feed.last_modified,
             "etag": feed.etag,
             "updated": updated,
+            "created": created,
         }
     }
 
