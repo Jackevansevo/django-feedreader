d7ffc8397fce1170fb893b809370070af95f17a1
Jack Evans <jack.evans@wegotpop.com>
2022-08-05 08:57:38 +0100

    removed django watchfiles

--- feedreader/settings.py
+++ feedreader/settings.py
@@ -115,7 +115,6 @@ MIDDLEWARE = [
 
 if DEBUG:
     INSTALLED_APPS.append("debug_toolbar")
-    INSTALLED_APPS.append("django_watchfiles")
 
     MIDDLEWARE.append("debug_toolbar.middleware.DebugToolbarMiddleware")
     INTERNAL_IPS = ["127.0.0.1"]
