f311f480b97fe5a029969cb552512359ea32450a
Jack Evans <[jack@evans.gb.net]>
2022-09-07 19:47:01 +0100

    Remove unused imports

--- feeds/views.py
+++ feeds/views.py
@@ -7,11 +7,10 @@ from django.conf import settings
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.core.cache import cache
 from django.core.paginator import Paginator
 from django.db import IntegrityError
 from django.db.models import Count, Exists, OuterRef
-from django.http import Http404, HttpRequest, HttpResponse
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import get_object_or_404, redirect, render
 from django.urls import reverse_lazy
 from django.utils import timezone
