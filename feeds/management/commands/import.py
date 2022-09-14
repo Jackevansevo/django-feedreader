1c42d09681c77fab05f71d7fd01ef4d9a24dd6a6
Jack Evans <jack@evans.gb.net>
2022-09-14 18:56:05 -0300

    use worker pool instead of semphaore

--- feeds/management/commands/import.py
+++ feeds/management/commands/import.py
@@ -7,13 +7,12 @@ import listparser
 from asgiref.sync import sync_to_async
 from django.contrib.auth.models import User
 from django.core.management.base import BaseCommand
+from rich.progress import Progress
 
 import feeds.crawler as crawler
 from feeds.models import Category, Feed, Subscription
 
 user = User.objects.first()
-
-limit = asyncio.Semaphore(200)
 
 
 def ingest(resp, parsed, favicon, category_name):
@@ -32,18 +31,13 @@ sync_ingest = sync_to_async(ingest)
 
 
 async def import_feed(client, feed):
-    async with limit:
-        print("Fetching:", feed["url"])
-        resp, parsed_feed, favicon = await crawler.Crawler(client, feed["url"]).crawl()
-        print("Got:", resp.url, resp.status_code)
-        await sync_ingest(resp, parsed_feed, favicon, feed["categories"][0][0])
-
-        if limit.locked():
-            print("Limit reached, sleeping temporarily")
-            await asyncio.sleep(0.5)
+    print("Fetching:", feed["url"])
+    resp, parsed_feed, favicon = await crawler.Crawler(client, feed["url"]).crawl()
+    print("Got:", resp.url, resp.status_code)
+    await sync_ingest(resp, parsed_feed, favicon, feed["categories"][0][0])
 
 
-async def main(infile):
+async def main(infile, workers):
 
     parsed = listparser.parse(infile.read())
 
@@ -51,14 +45,46 @@ async def main(infile):
     async for url in Feed.objects.values_list("url", flat=True):
         subscribed.add(url)
 
+    queue = asyncio.Queue()
+
+    for feed in parsed["feeds"]:
+        if feed["url"] not in subscribed:
+            queue.put_nowait(feed)
+
     async with httpx.AsyncClient(follow_redirects=True, timeout=60) as client:
-        await asyncio.gather(
-            *[
-                import_feed(client, feed)
-                for feed in parsed["feeds"]
-                if feed["url"] not in subscribed
-            ],
-        )
+
+        with Progress() as progress:
+
+            import_task = progress.add_task("Importing...", total=queue.qsize())
+
+            async def worker():
+                while True:
+                    # Get a "work item" out of the queue.
+                    feed = await queue.get()
+
+                    # Sleep for the "sleep_for" seconds.
+                    await import_feed(client, feed)
+
+                    progress.advance(import_task)
+
+                    # Notify the queue that the "work item" has been processed.
+                    queue.task_done()
+
+            # Create three worker tasks to process the queue concurrently.
+            tasks = []
+            for i in range(workers):
+                task = asyncio.create_task(worker())
+                tasks.append(task)
+
+            # Wait until the queue is fully processed.
+            await queue.join()
+
+            # Cancel our worker tasks.
+            for task in tasks:
+                task.cancel()
+
+            # Wait until all worker tasks are cancelled.
+            await asyncio.gather(*tasks, return_exceptions=True)
 
 
 class Command(BaseCommand):
@@ -66,6 +92,7 @@ class Command(BaseCommand):
         parser.add_argument(
             "infile", nargs="?", type=argparse.FileType("r"), default=sys.stdin
         )
+        parser.add_argument("--workers", nargs="?", type=int, default=100)
 
     def handle(self, *args, **options):
-        asyncio.run(main(options["infile"]))
+        asyncio.run(main(options["infile"], options["workers"]))
--- feeds/management/commands/update.py
+++ feeds/management/commands/update.py
@@ -9,7 +9,7 @@ from django.core.management.base import BaseCommand
 from django.utils import timezone
 from django.utils.http import http_date
 from urllib.parse import urljoin
-from rich.progress import track
+from rich.progress import Progress
 
 import feeds.parser as parser
 from feeds.models import Entry, Feed
@@ -26,70 +26,117 @@ async def fetch_feed(client, url, etag=None, last_modified=None):
     return await client.get(url, headers=headers)
 
 
-async def main(filter: Optional[str]):
+async def main(filter: Optional[str], workers):
+
+    feed_query = Feed.objects.values("url", "etag", "last_modified")
+    if filter is not None:
+        feed_query = feed_query.filter(url__icontains=filter)
 
     async with httpx.AsyncClient(follow_redirects=True, timeout=60) as client:
+        with Progress() as progress:
 
-        feed_query = Feed.objects.values("url", "etag", "last_modified")
-        if filter is not None:
-            feed_query = feed_query.filter(url__icontains=filter)
+            queue = asyncio.Queue()
 
-        tasks = [fetch_feed(client, **kwargs) async for kwargs in feed_query]
-        total_tasks = len(tasks)
+            results = asyncio.Queue()
 
-        for task in track(
-            asyncio.as_completed(tasks), total=total_tasks, description="Updating..."
-        ):
-            result = await task
+            async for feed in feed_query:
+                queue.put_nowait(feed)
 
-            lookup_url = result.url
-            if result.history:
-                lookup_url = result.history[0].url
+            fetch_task = progress.add_task("Fetching...", total=queue.qsize())
 
-            try:
-                feed = await Feed.objects.aget(url=lookup_url)
-            except Feed.DoesNotExist:
-                breakpoint()
+            async def worker(queue, client):
+                while True:
+                    # Get a "work item" out of the queue.
+                    feed = await queue.get()
 
-            update_fields = ["last_checked"]
-            feed.last_checked = timezone.now()
+                    # Sleep for the "sleep_for" seconds.
+                    resp = await fetch_feed(client, **feed)
 
-            # If we were redirected, update to the new URL
-            if result.history:
-                print(f"{feed.url} redirected -> {result.url}")
-                feed.url = str(result.url)
-                update_fields.append("url")
+                    # Notify the queue that the "work item" has been processed.
+                    queue.task_done()
 
-            if result.status_code == 200:
+                    results.put_nowait(resp)
 
-                parsed = parser.parse(io.BytesIO(result.content))
-                existing_entries = set()
+                    progress.advance(fetch_task)
 
-                async for link in Entry.objects.filter(
-                    feed__url=result.url
-                ).values_list("link", flat=True):
-                    existing_entries.add(link)
+            # Create three worker tasks to process the queue concurrently.
+            tasks = []
+            for i in range(workers):
+                task = asyncio.create_task(worker(queue, client))
+                tasks.append(task)
 
-                for entry in parsed["entries"]:
-                    link = entry.get("link")
-                    if (
-                        link is not None
-                        and urljoin(feed.link, link) not in existing_entries
-                    ):
-                        entry = parser.parse_feed_entry(entry, feed)
-                        await sync_to_async(entry.save)()
+            # Wait until the queue is fully processed.
+            await queue.join()
 
-                etag = result.headers.get("etag")
-                if etag is not None:
-                    update_fields.append("etag")
-                    feed.etag = etag
+            # Cancel our worker tasks.
+            for task in tasks:
+                task.cancel()
 
-                last_modified = result.headers.get("last-modified")
-                if last_modified is not None:
-                    update_fields.append("last_modified")
-                    feed.last_modified = dateutil.parser.parse(last_modified)
+            # Wait until all worker tasks are cancelled.
+            await asyncio.gather(*tasks, return_exceptions=True)
 
-            await sync_to_async(feed.save)(update_fields=update_fields)
+            async def process_results():
+                while True:
+                    result = await results.get()
+
+                    print("Got response from:", result.url)
+
+                    lookup_url = result.url
+                    if result.history:
+                        lookup_url = result.history[0].url
+
+                    try:
+                        feed = await Feed.objects.aget(url=lookup_url)
+                    except Feed.DoesNotExist:
+                        breakpoint()
+
+                    update_fields = ["last_checked"]
+                    feed.last_checked = timezone.now()
+
+                    # If we were redirected, update to the new URL
+                    if result.history:
+                        print(f"{feed.url} redirected -> {result.url}")
+                        feed.url = str(result.url)
+                        update_fields.append("url")
+
+                    if result.status_code == 200:
+
+                        parsed = parser.parse(io.BytesIO(result.content))
+                        existing_entries = set()
+
+                        async for link in Entry.objects.filter(
+                            feed__url=result.url
+                        ).values_list("link", flat=True):
+                            existing_entries.add(link)
+
+                        for entry in parsed["entries"]:
+                            link = entry.get("link")
+                            if (
+                                link is not None
+                                and urljoin(feed.link, link) not in existing_entries
+                            ):
+                                entry = parser.parse_feed_entry(entry, feed)
+                                await sync_to_async(entry.save)()
+
+                        etag = result.headers.get("etag")
+                        if etag is not None:
+                            update_fields.append("etag")
+                            feed.etag = etag
+
+                        last_modified = result.headers.get("last-modified")
+                        if last_modified is not None:
+                            update_fields.append("last_modified")
+                            feed.last_modified = dateutil.parser.parse(last_modified)
+
+                    await sync_to_async(feed.save)(update_fields=update_fields)
+
+                    results.task_done()
+
+            process_task = asyncio.create_task(process_results())
+
+            await results.join()
+
+            process_task.cancel()
 
 
 class Command(BaseCommand):
@@ -99,6 +146,7 @@ class Command(BaseCommand):
             nargs="?",
             type=str,
         )
+        parser.add_argument("--workers", nargs="?", type=int, default=100)
 
     def handle(self, *args, **options):
-        asyncio.run(main(options["filter"]))
+        asyncio.run(main(options["filter"], options["workers"]))
